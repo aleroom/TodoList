2ba0a6e01a1a440bd8a85659544913f77f3b73b4
aleroom <aleroom@yahoo.com>
2024-08-02 16:06:24 +0300

    add 'remove' todo

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,43 @@ const input = document.querySelector('#input');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
 const total = document.querySelector('#total');
+let i = 0;
 
 btn.addEventListener('click', (e) => {
-    console.log(input.value)
+    
 
-    result.innerHTML += `<li>${input.value}</li>`;
+    // result.innerHTML += `<li>${input.value}</li>`;
     if(input.value === '') return
     createDeleteElements(input.value)
     input.value = ''
 })
 
 function createDeleteElements(value) {
-    console.log(value)
+    i++ 
+
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    li.className = 'li';
+    li.textContent = value;
+
+    
+    btn.className = 'btn';
+    btn.textContent = "add";
+    li.appendChild(btn)
+
+    //remove todo
+    btn.addEventListener('click', (e) => {
+        i-- ;
+        total.textContent = i
+        result.removeChild(li) 
+             
+    })
+
+    li.addEventListener('click', e => {
+        li.classList.toggle('li-active')       
+    })
+
+    total.textContent = i
+    result.appendChild(li)
 }

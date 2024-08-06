a05d23489f56c2f9caede5a468b15f33cbfd585a
aleroom <ale.cherniavskaya@gmail.com>
2024-08-06 23:33:18 +0300

    add"inwork"

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('#input');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
+const inwork = document.querySelector('#inwork');
+const done = document.querySelector('#done');
 const total = document.querySelector('#total');
 let i = 0;
 
@@ -13,10 +15,15 @@ btn.addEventListener('click', (e) => {
     input.value = ''
 })
 
+btn2.addEventListener('click', (e) => {
+   
+})
+
 function createDeleteElements(value) {
     i++ 
     const li = document.createElement('li')
-    const btn = document.createElement('button')
+    const btn = document.createElement('button');
+    const btn2 = document.createElement("button2")
     
 
     li.className = 'li';
@@ -25,7 +32,10 @@ function createDeleteElements(value) {
     
     btn.className = 'btn';
     btn.textContent = "X";
-    li.appendChild(btn)
+    btn2.className = 'btn2';
+    btn2.textContent = "=>";
+    li.appendChild(btn);
+    li.appendChild(btn2);
 
     //remove todo
     btn.addEventListener('click', (e) => {
@@ -34,6 +44,26 @@ function createDeleteElements(value) {
         result.removeChild(li) 
              
     })
+
+    btn.addEventListener('click', (e) => {
+        i--;
+        // total.textContent = i
+        inwork.removeChild(li) 
+             
+    })
+
+    btn.addEventListener('click', (e) => {
+        i--;
+        // total.textContent = i
+        done.removeChild(li) 
+             
+    })
+    btn2.addEventListener('click', (e) => {
+        inwork.appendChild(li);
+    })
+    // btn2.addEventListener('click', (e) => {
+    //     done.appendChild(li);
+    // })
 
     li.addEventListener('click', e => {
         li.classList.toggle('li-active')       
